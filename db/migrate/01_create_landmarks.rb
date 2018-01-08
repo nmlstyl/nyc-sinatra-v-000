79e05ebae7536de755c22b2843937b18346c1512
joey michalina mariano <joey.michalina.mariano@gmail.com>
2018-01-08 08:05:49 +0000

    Done.

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,7 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise "Write CreateLandmarks migration here"
+  create_table :landmarks do |t|
+    t.string :name
+    t.integer :year_completed
+    t.integer :figure_id
+  end
 end
